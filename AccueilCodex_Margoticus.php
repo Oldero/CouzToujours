9a8fdb51ad29058d3d2a3537982b0ba88f621989
Neuhadub <46166629+Neuhadub@users.noreply.github.com>
2019-01-23 22:35:18 +0100

    Add files via upload

---
+++ AccueilCodex_Margoticus.php
@@ -0,0 +1,51 @@
+<?php
+//Le Codex, version, à corriger.
+
+    session_start ();
+?>
+  
+
+<!DOCTYPE php>
+
+<php>
+
+<head>
+    <?php include("../include/style.php"); ?>
+    <title>Codex Margoticus</title>
+</head>
+
+
+<body>
+	<pre>
+	<center>	La maison des Margots est un lieu de rencontres familiales où nous pouvons nous retrouver tout au long de l’année et où nous pouvons organiser des cousinades annuelles, AG, fête des Margots…  
+
+En dehors de ces temps forts, la maison des Margots est ouverte à tous les membres de la famille, éventuellement accompagnés d'amis, moyennant une contribution ou une adhésion.
+
+Les Margots sont loués en meublé par l’ASSOCIATION COUZ’ TOUJOURS, qui fixe les tarifs d’adhésion et les règles de vie.
+
+La présente notice d'utilisation porte sur la réservation et les tarifs des séjours, le fonctionnement général de la maison, et donne les éléments nécessaires pour l'ouverture et la fermeture de la maison.</center></pre> 
+	<link rel="stylesheet" href="menu.css"/>
+	<br/>
+	<br/>
+	<nav>
+	<ul>
+	<center>
+		<li><a href="Enarrivant.php">En arrivant</a></li><!--
+	    --><li><a href="Enpartant.php">En partant</a></li><!--
+		--><li><a href="Fonctionnementcourant.php">Fonctionnement courant</a></li><!--
+		--><li><a href="Quandvientl(hiver.php">Quand vient l'hiver</a></li><!--
+	</center>
+	</ul>
+	</nav>
+    <?php include("../include/entete.php"); ?>
+    <?php include("../include/laterale.php"); ?>
+
+    <section class="corps">
+    	<!--<iframe src="../docs/codex.pdf" width="800" height="600" align="middle"></iframe>-->
+    </section>
+
+    <?php include("../include/pieddepage.php"); ?>
+
+</body>
+
+</php>
