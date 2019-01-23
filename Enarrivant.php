0db0af3d696f6e5cd2d39f23af7cc3701103aed0
Neuhadub <46166629+Neuhadub@users.noreply.github.com>
2019-01-23 21:55:30 +0100

    Add files via upload

---
+++ Enarrivant.php
@@ -0,0 +1,55 @@
+<?php
+//Le Codex, version, à corriger.
+
+    session_start ();
+?>
+  
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <?php include("../include/style.php"); ?>
+    <title>En arrivant</title>
+</head>
+
+
+<body>
+	<p> Avant de venir : 
+Il est vivement recommandé d’apporter son linge de maison (draps de lit, serviettes de toilette, etc.). La maison est équipée de couvertures, ainsi que de draps et serviettes que vous pouvez utiliser à condition d’être sûr de pouvoir les laver et ranger avant de partir !
+
+Vérifier où sont les clés et se les faire envoyer par la poste ?
+
+GAZ
+- ouvrir les bouteilles situées à l'extérieur, à côté de la fenêtre de la cuisine.
+
+ELECTRICITE
+
+- l'interrupteur général est au dessus du frigo (vert visible = ouvert)
+- rebrancher et refermer le frigo+
+
+EAU 
+Voir annexe
+
+COURSES
+-	Marché jeudi matin à Yssingeaux
+-	Fromagerie d’Araules chez Gérintes
+-	Magasin de producteur « produits d’Yssi » à Yssingeaux en face de l’intermarché
+-	A COMPLETER…
+
+</p> 
+
+
+    <?php include("../include/entete.php"); ?>
+    <?php include("../include/laterale.php"); ?>
+
+    <section class="corps">
+    	<!--<iframe src="../docs/codex.pdf" width="800" height="600" align="middle"></iframe>-->
+    </section>
+
+    <?php include("../include/pieddepage.php"); ?>
+
+</body>
+
+</html>
---
+++ Enpartant.php
@@ -0,0 +1,52 @@
+<?php
+//Le Codex, version, à corriger.
+
+    session_start ();
+?>
+  
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <?php include("../include/style.php"); ?>
+    <title>En partant</title>
+</head>
+
+
+<body>
+	<p> Passer un coup de balais ou d’aspirateur dans chaque pièce utilisée (chambres, cuisine, salon, salle de bain, couloirs…)
+Laver les toilettes
+Vider les frigidaires
+Retirer le filtre de la cafetière
+Eteindre les chauffages et vider les ballons d’eau chaude si fermeture hivernale (voir Annexes)
+S’assurer que tous les feux (poeles, cheminée, four à pain) soient éteints. Vider les cendres dans le compost situé dans le potager (à l’arrière de la maison)
+Fermer le Jurinus
+Fermer les volets (planches en bois en bas. Rangées dans la souillarde – cf Plan)
+Merci de renouveler les stocks courants (PQ, sacs poubelle, liquide vaisselle, sucre, sel, et tout consommable qui vous avez aimé trouver en arrivant – exemple : chocolat et café !).
+
+Où sont les produits et aspi ?
+Aspirateurs => souillarde
+PQ => sous l’escalier
+Sacs poubelle => sous l’évier
+Balais et serpillères => derrière la porte de la cuisine
+Produits lavants du sol => sous l’évier de la cuisine
+Produit lave-vaisselle => placard au-dessus du lave-vaisselle
+Torchons => placard au-dessus du lave-vaisselle
+Allumettes
+</> 
+
+
+    <?php include("../include/entete.php"); ?>
+    <?php include("../include/laterale.php"); ?>
+
+    <section class="corps">
+    	<!--<iframe src="../docs/codex.pdf" width="800" height="600" align="middle"></iframe>-->
+    </section>
+
+    <?php include("../include/pieddepage.php"); ?>
+
+</body>
+
+</html>
